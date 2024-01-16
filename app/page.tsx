b5712ea15f5211573ef6e2e798f9b5ad878fc1f4
Sviatoslav <mursvyats@gmail.com>
2024-01-16 15:21:47 +0500

    Edited text size on mobiles

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <main
             className="flex min-h-screen flex-col items-center justify-between pt-0 mt-[var(--global-nav-collective-height)]">
             <div className="grid place-items-center h-[calc(100vh-var(--global-nav-collective-height))] text-primary">
-                <div className="text-4xl md:text-7xl lg:text-9xl text-center font-bold text-nowrap">
+                <div className="text-4xl sm:text-5xl md:text-7xl lg:text-9xl text-center font-bold text-nowrap">
                     <GradientText>Hi</GradientText>
                     <h1>My name is</h1>
                     <SpoilerTag hint={true}>
