f5a10fa631e340f97917ac8c47a8ce9c00a518b3
Sviatoslav <mursvyats@gmail.com>
2024-01-20 02:15:51 +0500

    style: removed unnecessary styling

--- components/ui/ProjectCard.tsx
+++ components/ui/ProjectCard.tsx
@@ -1,8 +1,6 @@
 'use client';
 import React from 'react';
 import {redirect} from "next/navigation";
-import airpods from '@/public/images/giphy.gif';
-import Image from 'next/image';
 
 interface IProjectCard {
     title: string;
@@ -20,7 +18,7 @@ const ProjectCard = ({title, description, children, link = '', isDeveloping = fa
 
         <div
             className={`w-[20rem] h-[20rem] bg-[#242424] rounded-2xl ${(isDeveloping) ? ('hover:bg-[#222] hover:border-[#333]') : ('hover:bg-[#333] hover:border-[#444]')} cursor-pointer transition-[background]
-                                    border-[1px] border-[#343434] hover:opacity-85 transition-opacity relative`}>
+                                    border-[1px] border-[#343434] hover:opacity-85 relative`}>
             <div className="rounded-[inherit]">
                 <div
                     className="bg-gradient-to-t from-black to-transparent rounded-[inherit] w-full h-[50%] top-1/2 absolute"></div>
