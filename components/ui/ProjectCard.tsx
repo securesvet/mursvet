8b3892b05836a5281569681209ad6e25c06e7b94
Sviatoslav <mursvyats@gmail.com>
2024-01-24 01:49:14 +0500

    feat: added transition when visible

--- components/ui/ProjectCard.tsx
+++ components/ui/ProjectCard.tsx
@@ -1,6 +1,6 @@
 'use client';
-import React from 'react';
-import {redirect} from "next/navigation";
+import React, {useEffect, useRef, useState} from 'react';
+import {useInView} from "react-intersection-observer";
 
 interface IProjectCard {
     title: string;
@@ -11,14 +11,13 @@ interface IProjectCard {
 }
 
 const ProjectCard = ({title, description, children, link = '', isDeveloping = false}: IProjectCard) => {
-    const handleClick = (path: string) => {
-        redirect(path)
-    }
+    const {ref: myRef, inView: cardIsVisible} = useInView();
+
     return (
         <a href={link}>
             <div
                 className={`w-[20rem] h-[20rem] bg-[#242424] rounded-2xl ${(isDeveloping) ? ('hover:bg-[#222] hover:border-[#333]') : ('hover:bg-[#333] hover:border-[#444]')} cursor-pointer transition-[background]
-                                        border-[1px] border-[#343434] hover:opacity-85 relative`}>
+                                        border-[1px] border-[#343434] hover:opacity-85 relative ${(cardIsVisible) ? ('transition-all translate-y-0 opacity-100') : ('opacity-0 transform translate-y-[20px]')}`}>
                 <div className="rounded-[inherit]">
                     <div
                         className="bg-gradient-to-t from-black to-transparent rounded-[inherit] w-full h-[50%] top-1/2 absolute"></div>
@@ -36,7 +35,8 @@ const ProjectCard = ({title, description, children, link = '', isDeveloping = fa
                     <h1 className="font-bold">{title}</h1>
                     <p className="text-lg text-[#aaa]">{description}</p>
                 </div>
-
+                {/* When this div is visible */}
+                <div ref={myRef} className="absolute top-[100px]"></div>
             </div>
         </a>
     );
