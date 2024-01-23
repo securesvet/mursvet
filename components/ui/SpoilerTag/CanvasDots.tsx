8a529713d736899addafdbedc7123ecbda99f8cd
Sviatoslav <mursvyats@gmail.com>
2024-01-23 19:10:05 +0500

    fix: now npm run build doesn't throw an error

--- components/ui/SpoilerTag/CanvasDots.tsx
+++ components/ui/SpoilerTag/CanvasDots.tsx
@@ -11,15 +11,14 @@ interface IDots {
 }
 
 const CanvasDots = ({width, height, children, density = ~~(width * height / 450 + 50), opacity}: IDots) => {
-    const canvas = useRef<HTMLCanvasElement>();
+    const canvas = useRef<HTMLCanvasElement>(null);
     let circles: { x: number, y: number, dx: number, dy: number, radius: number }[] = [];
 
     const createCircles = (amount: number = 10) => {
-        if (!canvas.current) return;
         for (let i = 0; i < amount; i++) {
             circles.push({
-                x: Math.random() * (canvas.current.width - 4) + 4,
-                y: Math.random() * (canvas.current.height - 4) + 4,
+                x: Math.random() * (width - 4) + 3,
+                y: Math.random() * (height - 4) + 3,
                 dx: Math.random() * 4 - 2,
                 dy: Math.random() * 4 - 2,
                 radius: Math.random() * 3 + 1
@@ -27,25 +26,40 @@ const CanvasDots = ({width, height, children, density = ~~(width * height / 450 
         }
     };
 
+    const updateCircles = () => {
+        circles.forEach(circle => {
+            circle.dx = Math.random() * 2 - 1;
+            circle.dy = Math.random() * 2 - 1;
+        });
+    };
+
+
+
     const draw = () => {
         if (!canvas.current) return;
         const ctx = canvas.current.getContext("2d");
         if (!ctx) return;
-        ctx.clearRect(0, 0, canvas.current.width, canvas.current.height);
+        ctx.clearRect(0, 0, width, height);
         ctx.fillStyle = '#fff';
         circles.forEach(circle => {
+            if (!canvas.current) return;
             ctx.beginPath();
             ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
             ctx.fill();
-            ctx.filter = `blur(${(circle.radius < 2) ? (0) : (1)}px)`
-            if (circle.x + circle.dx >= canvas.current.width - circle.radius || circle.x + circle.dx <= circle.radius) {
+            if (circle.radius < 3) {
+                ctx.filter = `blur(${1}px)`;
+            }
+            else {
+                ctx.filter = `none`
+            }
+            if (circle.x + circle.dx > width - circle.radius || circle.x + circle.dx < circle.radius) {
                 circle.dx = -circle.dx;
             }
-            if (circle.y + circle.dy >= canvas.current.height - circle.radius || circle.y + circle.dy <= circle.radius) {
+            if (circle.y + circle.dy > height - circle.radius || circle.y + circle.dy < circle.radius) {
                 circle.dy = -circle.dy;
             }
-            circle.x += Math.random() * circle.dx;
-            circle.y += Math.random() * circle.dy;
+            circle.x += circle.dx;
+            circle.y += circle.dy;
         });
         requestAnimationFrame(draw)
     };
@@ -53,14 +67,16 @@ const CanvasDots = ({width, height, children, density = ~~(width * height / 450 
     useEffect(() => {
         createCircles(density);
         draw();
-    }, [opacity]);
+    }, [width, height, opacity]);
+
+    setInterval(() => updateCircles(), 250);
 
     return (
         <div>
             {
                 (!opacity) ?
-                    <canvas ref={canvas} width={`${width}`}
-                            height={`${height}`}
+                    <canvas ref={canvas} width={width}
+                            height={height}
                             className={`${(opacity) ? ('hidden') : ('')} absolute overflow-hidden`}>
                     </canvas>
                     :
