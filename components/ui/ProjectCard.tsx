17a120982d5cb677987c19236dadde27b815dcb7
Sviatoslav <mursvyats@gmail.com>
2024-01-24 02:10:04 +0500

    refact: transition smoother + code style

--- components/ui/ProjectCard.tsx
+++ components/ui/ProjectCard.tsx
@@ -1,5 +1,6 @@
 'use client';
-import React, {useEffect, useRef, useState} from 'react';
+
+import React from 'react';
 import {useInView} from "react-intersection-observer";
 
 interface IProjectCard {
@@ -8,16 +9,24 @@ interface IProjectCard {
     children?: React.ReactNode;
     link?: string;
     isDeveloping?: boolean;
+    triggerOnShowPositon?: number;
 }
 
-const ProjectCard = ({title, description, children, link = '', isDeveloping = false}: IProjectCard) => {
+const ProjectCard = ({
+                         title,
+                         description,
+                         children,
+                         link = '',
+                         isDeveloping = false,
+                         triggerOnShowPositon = 0
+                     }: IProjectCard) => {
     const {ref: myRef, inView: cardIsVisible} = useInView();
 
     return (
         <a href={link}>
             <div
-                className={`w-[20rem] h-[20rem] bg-[#242424] rounded-2xl ${(isDeveloping) ? ('hover:bg-[#222] hover:border-[#333]') : ('hover:bg-[#333] hover:border-[#444]')} cursor-pointer transition-[background]
-                                        border-[1px] border-[#343434] hover:opacity-85 relative ${(cardIsVisible) ? ('transition-all translate-y-0 opacity-100') : ('opacity-0 transform translate-y-[20px]')}`}>
+                className={`w-[20rem] h-[20rem] bg-[#242424] rounded-2xl ${(isDeveloping) ? ('hover:bg-[#222] hover:border-[#333]') : ('hover:bg-[#333] hover:border-[#444]')} cursor-pointer transition-all
+                                        border-[1px] border-[#343434] hover:opacity-85 relative ${(cardIsVisible) ? ('translate-y-0 opacity-100') : ('opacity-0 transform translate-y-[20px]')}`}>
                 <div className="rounded-[inherit]">
                     <div
                         className="bg-gradient-to-t from-black to-transparent rounded-[inherit] w-full h-[50%] top-1/2 absolute"></div>
@@ -36,7 +45,7 @@ const ProjectCard = ({title, description, children, link = '', isDeveloping = fa
                     <p className="text-lg text-[#aaa]">{description}</p>
                 </div>
                 {/* When this div is visible */}
-                <div ref={myRef} className="absolute top-[100px]"></div>
+                <div ref={myRef} className={`absolute top-[${triggerOnShowPositon}px]`}></div>
             </div>
         </a>
     );
