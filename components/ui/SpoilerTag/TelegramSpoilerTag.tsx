44636c6dbccabfeda832e5d2f843bc64bd6d6f45
Sviatoslav <mursvyats@gmail.com>
2024-02-05 01:00:24 +0500

    feat: added breakpoint hook

--- components/ui/SpoilerTag/TelegramSpoilerTag.tsx
+++ components/ui/SpoilerTag/TelegramSpoilerTag.tsx
@@ -2,6 +2,7 @@
 
 import React, {useEffect, useRef, useState} from 'react';
 import {SparklesCore, ParticlesProps} from "@/components/ui/SparklesCore";
+import {useBreakpoint} from "@/hooks/useBreakpoint";
 
 type TelegramSpoilerTag = ParticlesProps & {
     children?: React.ReactNode;
@@ -11,6 +12,10 @@ const TelegramSpoilerTag = ({children, ...props}: TelegramSpoilerTag) => {
     const divUnderSparkles = useRef<HTMLDivElement>(null);
     const [isHidden, setIsHidden] = useState<Boolean>(true);
 
+    const { isSm } = useBreakpoint('sm');
+    const { isMd } = useBreakpoint('md');
+    const { isLg } = useBreakpoint('lg');
+
     return (
         <div className={`relative  hover:cursor-pointer`} onClick={() => setIsHidden(false)}>
             <SparklesCore
@@ -18,7 +23,7 @@ const TelegramSpoilerTag = ({children, ...props}: TelegramSpoilerTag) => {
                 minSize={1}
                 maxSize={1.4}
                 speed={1}
-                particleDensity={10}
+                particleDensity={isMd ? (200) : (1000)}
                 {...props}
             />
             <div ref={divUnderSparkles} className={`transition-opacity ${(isHidden) ? ('opacity-0') : ('opacity-100')}`}>
