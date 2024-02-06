53185004d8433a15a023bfe78443cefe7cafd328
Sviatoslav <mursvyats@gmail.com>
2024-02-06 22:41:26 +0500

    refact: removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import GradientText from "@/components/ui/GradientText";
 import {FaGithubAlt} from "react-icons/fa";
-import SpoilerTag from "@/components/ui/SpoilerTag/SpoilerTag";
 import Image from 'next/image'
 import ProjectCard from "@/components/ui/ProjectCard";
 import ScrolledDownAchievement from "@/components/ui/Achievements/ScrolledDownAchievement";
-import {SparklesCore} from "@/components/ui/SparklesCore";
 import TelegramSpoilerTag from "@/components/ui/SpoilerTag/TelegramSpoilerTag";
 
 
@@ -14,9 +12,12 @@ export default function Home() {
     return (
         <main
             className="flex min-h-screen flex-col items-center justify-between pt-0">
-            <div className="grid place-items-center h-[calc(100vh-var(--global-nav-collective-height))] text-primary">
+
+                <div className="grid place-items-center h-[calc(100vh-var(--global-nav-collective-height))] text-primary">
+
                 <div
                     className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-center font-bold text-nowrap">
+
                     <GradientText>Hello</GradientText>
                     <h1>My name is</h1>
                     <TelegramSpoilerTag>
@@ -30,6 +31,7 @@ export default function Home() {
 
             </div>
             <section id="projects">
+
                 <div
                     className="text-4xl sm:text-5xl md:text-7xl lg:text-8xl xl:text-9xl text-primary font-bold text-center">
                     <h1 className="mb-10">
--- components/ui/SpoilerTag/TelegramSpoilerTag.tsx
+++ components/ui/SpoilerTag/TelegramSpoilerTag.tsx
@@ -10,23 +10,27 @@ type TelegramSpoilerTag = ParticlesProps & {
 
 const TelegramSpoilerTag = ({children, ...props}: TelegramSpoilerTag) => {
     const divUnderSparkles = useRef<HTMLDivElement>(null);
-    const [isHidden, setIsHidden] = useState<Boolean>(true);
+    const [isSpoilerHidden, setIsSpoilerHidden] = useState<Boolean>(true);
 
     const { isSm } = useBreakpoint('sm');
     const { isMd } = useBreakpoint('md');
     const { isLg } = useBreakpoint('lg');
 
     return (
-        <div className={`relative  hover:cursor-pointer`} onClick={() => setIsHidden(false)}>
-            <SparklesCore
-                className={`w-full h-full absolute ${(!isHidden) ? ('hidden') : ('')}`}
-                minSize={1}
-                maxSize={1.4}
-                speed={1}
-                particleDensity={isMd ? (200) : (1000)}
-                {...props}
-            />
-            <div ref={divUnderSparkles} className={`transition-opacity ${(isHidden) ? ('opacity-0') : ('opacity-100')}`}>
+        <div className={`relative  hover:cursor-pointer`} onClick={() => setIsSpoilerHidden(false)}>
+            {
+                isSpoilerHidden ?
+                    <SparklesCore
+                        className={`w-full h-full absolute`}
+                        minSize={1}
+                        maxSize={1.4}
+                        speed={1}
+                        particleDensity={isMd ? (200) : (1000)}
+                        {...props}
+                    />
+                    : ('')
+            }
+            <div ref={divUnderSparkles} className={`transition-opacity ${(isSpoilerHidden) ? ('opacity-0') : ('opacity-100')}`}>
                 {children}
             </div>
         </div>
