2c6962d13d3d30681a59fbd0036b9423e6484f10
Sviatoslav <mursvyats@gmail.com>
2024-02-19 23:14:38 +0500

    refact: adding mobile support

--- components/ui/SpoilerTag/CanvasDots.tsx
+++ components/ui/SpoilerTag/CanvasDots.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, {memo, useEffect, useRef, useState} from 'react';
+import React, {memo, useEffect, useRef} from 'react';
 
 interface IDots {
     width: number;
@@ -11,9 +11,16 @@ interface IDots {
     velocity?: number;
 }
 
-const CanvasDots = ({width, height, children, density = ~~(width * height / 1000 + 50), opacity, velocity = 0.5}: IDots) => {
+const CanvasDots = ({
+                        width,
+                        height,
+                        children,
+                        density = ~~(width * height / 1000 + 50),
+                        opacity,
+                        velocity = 0.5
+                    }: IDots) => {
     const canvas = useRef<HTMLCanvasElement>(null);
-    let circles: {
+    const circles: {
         opacity: number,
         x: number, y: number, dx: number, dy: number, radius: number, phi: number
     }[] = [];
@@ -25,7 +32,7 @@ const CanvasDots = ({width, height, children, density = ~~(width * height / 1000
                 y: Math.random() * (height - 10) + 10,
                 dx: velocity * (Math.random() - 0.5),
                 dy: velocity * (Math.random() - 0.5),
-                radius: Math.random() * 1 + 2,
+                radius: Number((Math.random() + 2).toFixed(2)),
                 opacity: ~~Math.random() * 100,
                 phi: Math.random() * (2 * Math.PI),
             });
@@ -33,11 +40,18 @@ const CanvasDots = ({width, height, children, density = ~~(width * height / 1000
     };
 
 
-    const draw = () => {
+    const draw_2DCanvas = () => {
         if (!canvas.current) return;
+
         const ctx = canvas.current.getContext("2d");
         if (!ctx) return;
+        const ratio = Math.ceil(window.devicePixelRatio);
+        canvas.current.width = width * ratio;
+        canvas.current.height = height * ratio;
+        ctx.scale(ratio, ratio);
         ctx.clearRect(0, 0, width, height);
+        canvas.current.style.width = `${width}px`;
+        canvas.current.style.height = `${height}px`;
         circles.forEach(circle => {
             if (circle.x + circle.dx >= width - circle.radius || circle.x + circle.dx <= circle.radius) {
                 circle.dx = -circle.dx;
@@ -52,28 +66,25 @@ const CanvasDots = ({width, height, children, density = ~~(width * height / 1000
             ctx.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
             ctx.fillStyle = '#fff';
             circle.phi = 0.005 + circle.phi % (2 * Math.PI);
-            circle.opacity = 40 * (Math.sin(circle.phi) + 1) + 10;
+            circle.opacity = 50 * (Math.sin(circle.phi) + 1);
             ctx.filter = `opacity(${circle.opacity}%)`
             ctx.fill();
         });
-        requestAnimationFrame(draw)
+        requestAnimationFrame(draw_2DCanvas)
     };
 
     useEffect(() => {
         createCircles(density);
-        draw();
+        draw_2DCanvas();
     }, [width, height, opacity]);
 
     return (
         <div>
             {
-                (!opacity) ?
-                    <canvas ref={canvas} width={width}
-                            height={height}
-                            className={`${(opacity) ? ('hidden') : ('')} absolute overflow-hidden`}>
-                    </canvas>
-                    :
-                    ('')
+                (!opacity) &&
+                <canvas ref={canvas} width={width}
+                        height={height}
+                        className={`${(opacity) && ('hidden')} absolute overflow-hidden`}/>
             }
             {children}
         </div>
