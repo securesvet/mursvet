e614849885eb111f4856033a3c9f7ae2ec5cad14
Sviatoslav <mursvyats@gmail.com>
2024-02-21 22:49:15 +0500

    refact: deleted unused imports

--- components/ui/SpoilerTag/CanvasDots.tsx
+++ components/ui/SpoilerTag/CanvasDots.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, {memo, useEffect, useRef} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {getRandomInt, getRandomArbitrary} from "@/lib/utils";
 
 interface IDots {
