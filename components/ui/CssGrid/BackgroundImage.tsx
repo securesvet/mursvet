457196b5d4d018bc899f7657b3af9153b6e8fcc0
Sviatoslav <mursvyats@gmail.com>
2024-05-03 02:50:10 +0500

    Added new bento-grid to a site

--- components/ui/CssGrid/BackgroundImage.tsx
+++ components/ui/CssGrid/BackgroundImage.tsx
@@ -2,19 +2,19 @@ import 'react';
 import { twMerge } from 'tailwind-merge';
 
 type BackgroundImageProps = {
+    imageUrl?: string | undefined;
     url?: string | undefined;
-    onClick?: () => void;
 }
 
-const BackgroundImage = ({url, onClick}:BackgroundImageProps) => {
+const BackgroundImage = ({imageUrl, url}:BackgroundImageProps) => {
     return (
         <>
-        <div className={twMerge('min-h-full rounded-[inherit] box-border')} onClick={onClick}>
+        <div className={twMerge('w-full h-full rounded-[inherit] box-border relative')}>
 
-            {(url !== undefined) ?
-                <img src={url} className='w-full h-full object-cover rounded-[inherit] opacity-50 hover:opacity-25 transition-opacity hover:cursor-pointer'/>
+            {imageUrl ?
+                <img src={imageUrl} className='w-full h-full object-cover rounded-[inherit] opacity-50 hover:opacity-25 transition-opacity hover:cursor-pointer'/>
                 :
-                ''
+                <div className="w-full h-full bg-neutral-500 hover:bg-neutral-700 hover:cursor-pointer hover:transition-colors rounded-[inherit"/>
             }
         </div>
         </>
--- components/ui/CssGrid/CssGrid.tsx
+++ components/ui/CssGrid/CssGrid.tsx
@@ -1,33 +1,33 @@
-'use client';
-
 import { twMerge } from "tailwind-merge";
 import data from '@/public/data/projects.json';
 import { useState } from "react";
 import BackgroundImage from "./BackgroundImage";
-import { redirect } from "next/dist/server/api-utils";
+import { useRouter } from "next/navigation";
 
 type projectDataType = {
     title: string;
     value: string;
     bgImage?: string | undefined; 
-    link: string;
+    link: string | undefined;
 };
 
 const CssGrid = () => {
+    const router = useRouter();
     const boxStyle = 'border-2 border-neutral-500 rounded-xl p-0 flex flex-col items-center justify-center';
     const [projectData, setProjectData] = useState([...data.data]);
+
     return (
     <div className="grid md:grid-cols-3 auto-rows-[300px] gap-2">
         {projectData.map((item, i) => (
-            <div key={i}  
-            className={twMerge(boxStyle, `${i === 3 || i === 5 ? 'md:col-span-2' : ''} ${i === 2 ? 'md:row-span-2' : ''} relative`)}>
+            <div key={i}
+            onClick={() => router.push(item.link)}
+            className={twMerge(boxStyle, `${i === 3 ? 'md:col-span-2' : ''} ${i === 2 ? 'md:row-span-2' : ''} ${i === 4 ? 'md:col-span-3' : ''} relative`)}>
                 <div className="absolute z-10">
                     <h2 className="text-xl text-gray-300 opacity-100">{item.title}</h2>
                     <p className="font-bold text-2xl opacity-100">{item.value}</p>
                 </div>
-                <BackgroundImage url={`/images/${item.bgImage}`} onClick={() => function() {
-                redirect(item.link, 'replace')
-            }}/>
+                <BackgroundImage imageUrl={`${item.bgImage ? `/images/${item.bgImage}` : ''}`} />
+            
             </div>
         ))}
 
