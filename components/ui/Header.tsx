3338f89b5a17c79fc901b1cb46deb344ddbd66c1
Sviatoslav <mursvyats@gmail.com>
2024-05-13 00:00:20 +0500

    feat: added back to home when on logo click

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -12,9 +12,11 @@ const Header = () => {
             className="fixed min-w-full top-0 z-10 bg-[#161616] border-b-[1px] border-border backdrop-filter backdrop-blur-lg bg-opacity-30 z-10">
             <div className="min-w-full px-4">
                 <div className="flex items-center justify-between h-[var(--global-nav-collective-height)] text-primary">
+                    <a href="/">
                     <span className={`${jetBrainsMono.className} text-2xl font-semibold`}>
                         MUR
                     </span>
+                    </a>
                     <ul className="flex justify-end space-x-4">
                         <li><a href="/#">Home</a></li>
                         <li><a href="/#projects">Projects</a></li>
